0eccb4daca29eeaade80dd03a42e20877af7b950
yolandadidas <yoladidas@gmail.com>
2025-05-03 17:39:41 +0300

    Added CORS policy

--- studentsapi/settings.py
+++ studentsapi/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = ['*', 'localhost']
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
